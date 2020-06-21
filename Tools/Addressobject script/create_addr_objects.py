eb354ce5e4eac4d1333824c08567d290ede16988
Per Rosenlind <rosenlind89@live.se>
2020-06-21 11:02:18 +0200

    Updated script

--- Tools/Addressobject script/create_addr_objects.py
+++ Tools/Addressobject script/create_addr_objects.py
@@ -17,23 +17,23 @@ for line in addresslist.splitlines():
     addrtype = fields[1]
     address = fields[2]
     netmask = fields[3]
-    assoicatedinterface = fields[4]
+    associatedinterface = fields[4]
     output.write('\n')
     output.write('config firewall address')
     output.write('\n')
-    output.write('edit ')
+    output.write('edit "')
     output.write(name)
-    output.write('\n')
+    output.write('"\n')
     output.write('set type ')
     output.write(addrtype)
     output.write('\n')
-    output.write('set subnet')
+    output.write('set subnet ')
     output.write(address)
     output.write(' ')
     output.write(netmask)
     output.write('\n')
     output.write('set associated-interface ')
-    output.write(assoicatedinterface)
+    output.write(associatedinterface)
     output.write('\n')
     output.write('end')
     output.write('\n')
